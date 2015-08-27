c177ac528161c991f45d5e246eb044dea1db736d
Andreas von Holy <andreas409@gmail.com>
2015-08-27 20:10:17 +0200

    parser implemented and partially working, confused about how to use it exactlyt

--- lex_ula.py
+++ lex_ula.py
@@ -2,116 +2,128 @@ __author__ = 'VHLAND002'
 
 import lex
 
+tokens = ('ID', 'FLOAT_LITERAL', 'ADD', 'SUB', 'MULT', 'DIV', 'EQUAL', 'OBRACKET', 'CBRACKET', 'WHITESPACE', 'MLCOMMENT', 'SLCOMMENT')
 
-class Lexer(object):
-
-
-
-    #tokens
-    tokens = ('ID', 'FLOAT_LITERAL', 'ADD', 'SUB', 'MULT', 'DIV', 'EQUAL', 'OBRACKET', 'CBRACKET', 'WHITESPACE', 'MLCOMMENT', 'SLCOMMENT')
-    # Regular expression rules for simple tokens
-
-    t_ADD = r'\@'
-    t_SUB = r'\$'
-    t_MULT = r'\#'
-    t_DIV = r'\&'
-    t_EQUAL = r'\='
-    t_OBRACKET = r'\('
-    t_CBRACKET = r'\)'
-
-
-    #regular expressions
-
-    def t_ID(self,t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.value = '' + t.value
-        return t
-
-
-    def t_FLOAT_LITERAL(self,t):
-        #if we find a digit and can have 1 or more iterations of digits
-        #r'\d+\.?\d+'
-
-        r'[+-]?\d+(\.\d+)?([eE][+-]?\d+)?'
-        #t.value = float(t.value)
-        return t
-
-
-    def t_SLCOMMENT(self,t):
-        #means we check for // and go up until the end of the line ie \n (specified by the .) and then have 0 or more iterations of this (*)
-        r'//.*'
-        t.value = "COMMENT"
-        return t
-
-
-    def t_MLCOMMENT(self,t):
-        #check for this pattern: /* any number of things other than */ [^*/] up until */
-        r'\/\*+[^*/]+\*+\/'
-        t.value = "COMMENT"
-        return t
-
-    '''
-    def t_newline(t):
-        r'\n+'
-        t_WHITESPACE(t)
-        t.lexer.lineno += len(t.value)
-    '''
-
-    def t_WHITESPACE(self,t):
-        #check for either of the characters and mark them as whitespace
-        r'[ \t\r\n]+'
-        t.value = 'WHITESPACE'
-        return t
-
-
-    def t_error(self,t):
-        print("Bad char '%s'" % t.value[0])
-        t.lexer.skip(1)
+#class Lexer(object):
 
 
 
+#tokens
+#tokens = ('ID', 'FLOAT_LITERAL', 'ADD', 'SUB', 'MULT', 'DIV', 'EQUAL', 'OBRACKET', 'CBRACKET', 'WHITESPACE', 'MLCOMMENT', 'SLCOMMENT')
+# Regular expression rules for simple tokens
 
-    #builds the lexer
-    def build(self,**kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
+t_ADD = r'\@'
+t_SUB = r'\$'
+t_MULT = r'\#'
+t_DIV = r'\&'
+t_EQUAL = r'\='
+t_OBRACKET = r'\('
+t_CBRACKET = r'\)'
 
+#regular expressions
 
-
-    #testing method which received some input to be passed through
-    def test(self, data):
-        # Test it out
-
-
-        # Give the lexer some input
-        self.lexer.input(data)
-
-        # Tokenize
-        while True:
-            tok = self.lexer.token()
-            if not tok:
-                break
-            print(tok)
+# def t_ID(self,t)
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.value = '' + t.value
+    return t
 
 
-    def importFile(self, fileName):
-        inFile = open(fileName, 'r')
-        data = inFile.read()
-        inFile.close()
-        # Give the lexer some input
-        self.lexer.input(data)
+# def t_FLOAT_LITERAL(self,t)
+def t_FLOAT_LITERAL(t):
+    #if we find a digit and can have 1 or more iterations of digits
+    #r'\d+\.?\d+'
 
-        outFile = open(fileName[0:-4] + '_1.tkn', 'w')
+    r'[+-]?\d+(\.\d+)?([eE][+-]?\d+)?'
+    #t.value = float(t.value)
+    return t
 
-        # Tokenize
-        while True:
-            tok = self.lexer.token()
-            if not tok:
-                break
-            if tok.type == 'FLOAT_LITERAL' or tok.type == 'ID':
-                 outFile.write(tok.type + "," + str(tok.value) + '\n')
-                 print(tok.type + "," + str(tok.value))
-            else:
-                outFile.write(str(tok.value) + '\n')
-                print(str(tok.value))
 
-        outFile.close()
+# def t_SLCOMMENT(self,t):
+def t_SLCOMMENT(t):
+    #means we check for // and go up until the end of the line ie \n (specified by the .) and then have 0 or more iterations of this (*)
+    r'//.*'
+    t.value = "COMMENT"
+    return t
+
+
+#def t_MLCOMMENT(self,t):
+def t_MLCOMMENT(t):
+    #check for this pattern: /* any number of things other than */ [^*/] up until */
+    r'\/\*+[^*/]+\*+\/'
+    t.value = "COMMENT"
+    return t
+
+'''
+def t_newline(t):
+    r'\n+'
+    t_WHITESPACE(t)
+    t.lexer.lineno += len(t.value)
+'''
+
+#def t_WHITESPACE(self,t):
+def t_WHITESPACE(t):
+    #check for either of the characters and mark them as whitespace
+    r'[ \t\r\n]+'
+    t.value = 'WHITESPACE'
+    return t
+
+
+#def t_error(self,t):
+def t_error(t):
+    print("Bad char '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+
+lexer = lex.lex()
+
+
+
+'''
+#builds the lexer
+def build(self,**kwargs):
+    self.lexer = lex.lex(module=self, **kwargs)
+
+'''
+
+'''
+#testing method which received some input to be passed through
+def test(self, data):
+    # Test it out
+
+
+    # Give the lexer some input
+    self.lexer.input(data)
+
+    # Tokenize
+    while True:
+        tok = self.lexer.token()
+        if not tok:
+            break
+        print(tok)
+'''
+'''
+def importFile(self, fileName):
+    inFile = open(fileName, 'r')
+    data = inFile.read()
+    inFile.close()
+    # Give the lexer some input
+    self.lexer.input(data)
+
+    outFile = open(fileName[0:-4] + '_1.tkn', 'w')
+
+    # Tokenize
+    while True:
+        tok = self.lexer.token()
+        if not tok:
+            break
+        if tok.type == 'FLOAT_LITERAL' or tok.type == 'ID':
+            outFile.write(tok.type + "," + str(tok.value) + '\n')
+            print(tok.type + "," + str(tok.value))
+        else:
+            outFile.write(str(tok.value) + '\n')
+            print(str(tok.value))
+
+    outFile.close()
+'''
--- parse_ula.py
+++ parse_ula.py
@@ -1,6 +1,94 @@
 __author__ = 'VHLAND002'
 
-import yacc
+import ply.yacc as yacc
+
+from lex_ula import tokens
+import lex_ula
 
 
 
+
+'''
+# program -> statement*
+def p_program_statement(p):
+    'program : statement*'
+    p[0] = p[1]
+'''
+
+
+# statement -> ID = expression
+def p_statement_expression(p):
+    'statement : ID EQUAL expression'
+    p[0] = p[1] = p[3]
+
+
+# Expression -> Expression @ Term
+def p_expression_add(p):
+    'expression : expression ADD term'
+    p[0] = float(p[1]) + float(p[3])
+
+
+# Expression -> Expression $ Term
+def p_expression_sub(p):
+    'expression : expression SUB term'
+    p[0] = float(p[1]) - float(p[3])
+
+
+# Expression -> Term
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+
+# Term -> Term # Factor
+def p_term_mult(p):
+    'term : term MULT factor'
+    p[0] = float(p[1]) * float(p[3])
+
+
+# Term -> Term & Factor
+def p_term_div(p):
+    'term : term DIV factor'
+    p[0] = float(p[1]) / float(p[3])
+
+
+# Term -> Factor
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+
+# Factor -> (Expression)
+def p_factor_expr(p):
+    'factor : OBRACKET expression CBRACKET'
+    p[0] = p[2]
+
+
+# Factor -> float
+def p_factor_float_literal(p):
+    'factor : FLOAT_LITERAL'
+    p[0] = float(p[1])
+
+
+# Factor -> identifier
+def p_factor_id(p):
+    'factor : ID'
+    p[0] = p[1]
+
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input('calc > ')
+    except EOFError:
+        break
+
+    if not s: continue
+    result = parser.parse(s,lexer = lex_ula.lexer)
+    print(result)
